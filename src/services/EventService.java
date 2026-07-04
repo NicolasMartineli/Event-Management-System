10243499fad341bdbada85943c48e6ef84bc001c
Nicolas <nicolaslmartineli@gmail.com>
2026-07-04 16:15:13 -0300

    feat: add method to list future events using stream filter

--- src/services/EventService.java
+++ src/services/EventService.java
@@ -1,5 +1,18 @@
 package services;
 
-public class EventService  {
+import entities.Event;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+
+public class EventService {
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public void listFutureEvents(List<Event> events) {
+        events.stream()
+                .filter(event -> event.getData().isAfter(LocalDate.now()) || event.getData().isEqual(LocalDate.now()))
+                .forEach(event -> System.out.println(
+                        event.getId() + " - " + event.getName() + " - " + event.getData().format(fmt)));
     }
+}
