f6f7cea59ca7f2747805d46c87cead64eb34a39b
Nicolas <nicolaslmartineli@gmail.com>
2026-07-05 13:57:45 -0300

    feat: add method to search events by name (case-insensitive)

--- src/services/EventService.java
+++ src/services/EventService.java
@@ -11,6 +11,7 @@ public class EventService {
     private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
     public void listFutureEvents(List<Event> events) {
+
         events.stream()
                 .filter(event -> event.getData().isAfter(LocalDate.now()) || event.getData().isEqual(LocalDate.now()))
                 .forEach(event -> System.out.println(
@@ -18,12 +19,23 @@ public class EventService {
     }
 
     public void listEventsperDate(List<Event> events) {
+
         Comparator<Event> comp = (e1, e2) -> e1.getData().compareTo(e2.getData());
+
         events.sort(comp);
         events.forEach(event -> System.out.println(event.getId()
                 + " - " + event.getName() + " - " + event.getData().format(fmt)));
 
-
     }
 
+    public void findEventsByName(List<Event> events, String word) {
+
+        events.stream()
+                .filter(event -> event.getName().toLowerCase().contains(word.toLowerCase()))
+                .forEach(event -> System.out.println(event.getId() + " - " + event.getName() + " - " + event.getData().format(fmt)));
+
+
+    }
 }
+
+
